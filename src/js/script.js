ef473402332f54654287f71bc98f17b1bb855c80
m-iwasaki96 <iwasaki.ocu96@gmail.com>
2023-09-29 01:02:10 +0900

    JavaScriptコンソールエラー解消他

--- src/js/script.js
+++ src/js/script.js
@@ -54,59 +54,61 @@ const newsTrigger = {
 
 const mm = gsap.matchMedia();
 mm.add('(min-width:769px)', function () {
-  gsap.to('.service__title',{clipPath:'inset(0 0 0 0%)',duration:2,ease:custom,scrollTrigger:serviceTrigger});
+  if ($("body").hasClass('js-top')) {
+    gsap.to('.service__title',{clipPath:'inset(0 0 0 0%)',duration:2,ease:custom,scrollTrigger:serviceTrigger});
+    gsap.to('.works__title',{clipPath:'inset(0 0% 0 0)',duration:2,ease:custom,scrollTrigger:worksTrigger});
+    gsap.to('.news__title',{clipPath:'inset(0 0% 0 0)',duration:2,ease:custom,scrollTrigger:newsTrigger});
+  
+    gsap.fromTo('.about__img',
+      {opacity:0, x:-30},
+      {opacity:1, x:0, duration:1, scrollTrigger:{
+        trigger:'.about__img',
+        start:'top 70%',
+    }});
+    gsap.fromTo('.about__contents',
+      {opacity:0, x:30},
+      {opacity:1, x:0, duration:1, scrollTrigger:{
+        trigger:'.about__contents',
+        start:'top 70%',
+    }});
+  
+    gsap.fromTo('.service__card',
+      {opacity:0, y:20},
+      {opacity:1, y:0, duration:1, stagger:.3, scrollTrigger:{
+        trigger:'.service__card',
+        start:'top 75%',
+    }});
+  
+    gsap.fromTo('.works__item',
+      {opacity:0, y:20},
+      {opacity:1, y:0, duration:1, stagger:.5, scrollTrigger:{
+        trigger:'.works__item',
+        start:'top 75%',
+    }});
+  } else if ($("body").hasClass('js-listPage')) {
+    gsap.fromTo('.list__categories',
+      {opacity:0, y:20},
+      {opacity:1, y:0, duration:1.5, scrollTrigger:{
+        trigger:'.list__categories',
+        start:'top 75%',
+    }});
+  
+    gsap.fromTo('.list__menu-item',
+      {opacity:0, y:20},
+      {opacity:1, y:0, duration:.3, stagger:.3, scrollTrigger:{
+        trigger:'.list__menu-item',
+        start:'top 75%',
+    }});
+  }
 
-  gsap.to('.works__title',{clipPath:'inset(0 0% 0 0)',duration:2,ease:custom,scrollTrigger:worksTrigger});
-
-  gsap.to('.news__title',{clipPath:'inset(0 0% 0 0)',duration:2,ease:custom,scrollTrigger:newsTrigger});
-
-  gsap.fromTo('.about__img',
-    {opacity:0, x:-30},
-    {opacity:1, x:0, duration:1, scrollTrigger:{
-      trigger:'.about__img',
-      start:'top 70%',
-  }});
-  gsap.fromTo('.about__contents',
-    {opacity:0, x:30},
-    {opacity:1, x:0, duration:1, scrollTrigger:{
-      trigger:'.about__contents',
-      start:'top 70%',
-  }});
-
-  gsap.fromTo('.service__card',
-    {opacity:0, y:20},
-    {opacity:1, y:0, duration:1, stagger:.3, scrollTrigger:{
-      trigger:'.service__card',
-      start:'top 75%',
-  }});
-
-  gsap.fromTo('.works__item',
-    {opacity:0, y:20},
-    {opacity:1, y:0, duration:1, stagger:.5, scrollTrigger:{
-      trigger:'.works__item',
-      start:'top 75%',
-  }});
-
-  gsap.fromTo('.list__categories',
-    {opacity:0, y:20},
-    {opacity:1, y:0, duration:1.5, scrollTrigger:{
-      trigger:'.list__categories',
-      start:'top 75%',
-  }});
-
-  gsap.fromTo('.list__menu-item',
-    {opacity:0, y:20},
-    {opacity:1, y:0, duration:.3, stagger:.3, scrollTrigger:{
-      trigger:'.list__menu-item',
-      start:'top 75%',
-  }});
-
-  // 下層ページMVアニメーション
-  gsap.fromTo('.lower-mv__title',
-    {opacity:0, x:-30},
-    {opacity:1, x:0, duration:1, delay:.5});
-
+  if(document.querySelector('.lower-mv__title', 'lower-mv__img') !== null) {
+    // 下層ページMVアニメーション
+    gsap.fromTo('.lower-mv__title',
+      {opacity:0, x:-30},
+      {opacity:1, x:0, duration:1, delay:.5});
+  
     gsap.fromTo('.lower-mv__img',
-    {opacity:0, x:30},
-    {opacity:1, x:0, duration:2});
+      {opacity:0, x:30},
+      {opacity:1, x:0, duration:2});
+  }
 });
